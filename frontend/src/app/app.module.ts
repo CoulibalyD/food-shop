8854e192fb88dea4893d0448409ee109d35d146e
Dra <dramane.coulibalyd636@gmail.com>
2023-11-07 03:06:21 +0000

    first commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { StarRatingComponent } from './components/partials/star-rating/star-rating.component';
+import { SearchComponent } from './components/partials/search/search.component';
+import { TagsComponent } from './components/partials/tags/tags.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    StarRatingComponent,
+    SearchComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Food } from 'src/app/models/Food';
+import { FoodService } from 'src/app/services/food.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  foods: Food[] = [];
+  constructor(private foodService: FoodService, activatedRoute:ActivatedRoute) {
+    activatedRoute.params.subscribe((params) => {
+      if (params.searchTerm) 
+      this.foods = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      else if(params.tag)
+      this.foods = this.foodService.getAllFoodsByTag(params.tag);
+      else
+      this.foods = this.foodService.getAll();
+    })
+  }
+  ngOnInit(): void {}
+}
